7fb6bb9198eb745e531e989d5eebf1285ef61f3f
Sandra Sandeep <sandrasandeep1@gmail.com>
2024-06-01 20:20:26 -0700

    ability to add items to recurring... sort of?

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+// import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { FormControl, FormLabel } from "@mui/material";
 
+function BasicTextFields() {
+  const [item, setItem] = useState("");
+  const saveNewItem = useCallback(() => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ item }),
+    };
+
+    fetch("/api/add_recurring_item", requestOptions);
+    // .then((response) => response.json())
+    // .then((data) => setPostId(data.id));
+  }, [item]);
+
+  return (
+    <FormControl onSubmit={saveNewItem}>
+      <FormLabel>Enter Grocery Item</FormLabel>
+      <TextField
+        label="item"
+        onChange={(e) => setItem(e.target.value)}
+        variant="outlined"
+        value={item}
+      ></TextField>
+      <Button type="submit">Submit</Button>
+    </FormControl>
+  );
+}
 function App() {
-  const [data, setData] = useState([{}]);
+  const [recurringList, setRecurringList] = useState([{}]);
 
   useEffect(() => {
-    fetch("/members")
+    fetch("/api/get_recurring_list")
       .then((result) => result.json())
       .then((result) => {
-        setData(result);
+        setRecurringList(result);
         console.log(result);
       });
   }, []);
+
   return (
     <div>
-      {typeof data.members === "undefined" ? (
+      {typeof recurringList.items === "undefined" ? (
         <p>Loading...</p>
       ) : (
-          data.members.map((member, i) => <p key={i}>{member}</p>)
+        <div>
+          {/* (recurringList.items.map((item, i) => <p key={i}>{item}</p>)) */}
+          <h1>Add To List:</h1>
+          <BasicTextFields />
+        </div>
+        // data.members.map((member, i) => <p key={i}>{member}</p>)
       )}
     </div>
   );
